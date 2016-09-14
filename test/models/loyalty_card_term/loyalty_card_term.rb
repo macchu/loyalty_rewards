bc7e93f60ca69ee3bc9696fa8de3b6d72431268a
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-09-14 10:18:34 -0500

    Tweaking fixtures.

--- test/models/loyalty_card_term/loyalty_card_term.rb
+++ test/models/loyalty_card_term/loyalty_card_term.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class LoyaltyCardTerm < ActiveSupport::TestCase
   def setup
-    @loyalty_card_term_for_coop = loyalty_card_terms(:loyalty_card_term_for_coop)
+    @loyalty_card_term_for_coop = loyalty_card_terms(:loyalty_card_terms_for_coop)
   end
 
   test 'ActiveRecord stuff' do
-    assert_equal 1, LoyaltyCardTerm.count
+    assert_equal "Free mug", @loyalty_card_term_for_coop.reward_description
   end
 
 end
