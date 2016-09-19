8f9e0b9c8147dcb85878c7b9e40561c21c626a50
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-09-19 11:21:59 -0500

    Linking check_ins to check_in_strategies.

--- test/models/check_in/sms_with_code_test.rb
+++ test/models/check_in/sms_with_code_test.rb
@@ -4,16 +4,13 @@ class SMSWithCodeTest < ActiveSupport::TestCase
   def setup
     @julieta = patrons(:julieta)
     @sms_with_code = check_in_strategies(:sms_with_code)
-    CheckIn.create(check_in_strategy: :sms_with_code, phone_number: @julieta.digit_only_phone_number)
+    CheckIn.create( check_in_strategy: CheckInStrategy.find_by_name(:sms_with_code), 
+                    phone_number: @julieta.digit_only_phone_number)
   end
 
   test "Creates a CheckIn using the :sms_with_code strategy" do
     check_in = CheckIn.last
     assert_equal @sms_with_code, check_in.strategy
-  end
-
-  test ".store" do
-    assert_equal @check_in.store, @coop
   end
 
 end
