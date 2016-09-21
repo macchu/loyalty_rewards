024f2304ed0b2663f0022d73bb3537dd93525c68
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-09-21 15:44:30 -0500

    Test the check_in message 'controller' with an enrollment scenario.

---
+++ test/services/check_in_message_controller/enrollment_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class CheckInMessageControllerTest
+  class EnrollmentTest < ActionDispatch::IntegrationTest
+    def setup
+      @controller = CheckInMessageController.new(message)
+    end
+
+    test 'An unknown phone number starts signup process.' do 
+
+    
+    end
+
+  end
+end
