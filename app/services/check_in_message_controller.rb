83fe39d1f21e390c5c4e9ddd18cd1dea9b98d98b
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-09-21 15:43:31 -0500

    Changed this unit test to a controller test.

--- app/services/check_in_message_controller.rb
+++ app/services/check_in_message_controller.rb
@@ -3,8 +3,8 @@
 class CheckInMessageController
   def initialize(message)
     parsed_message = ParseEmailMessage.new(message)
-
-    @check_in = CheckIn.new(parsed_message.check_in_params)
+    params = { phone_number: parsed_message.sender_local_part }
+    @check_in = CheckIn.create(params)
 
     case @check_in.status
     when :check_in
@@ -12,7 +12,7 @@ class CheckInMessageController
     when :enroll
       @check_in_id = @check_in.id
       @new_patron = Patron.new(phone_number: @check_in.phone_number)
-      send_complete_check_in_message(@new_patron)
+      #send_complete_check_in_message(@new_patron)
     
     end
   end
