0beca8de17e9d51608f7c3b12f34ef3edf8e8701
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-09-22 11:11:02 -0500

    This view asks patrons for their full name.

--- app/mailers/patron_enrollment_mailer.rb
+++ app/mailers/patron_enrollment_mailer.rb
@@ -1,2 +1,9 @@
 class PatronEnrollmentMailer < ApplicationMailer
+  default from: 'no-reply@example.com',
+          return_path: 'system@example.com'
+
+  def provide_name(patron_address)
+    logger.info " #{self.class.to_s}##{__method__.to_s}: #{patron_address}"
+    mail(to: patron_address,
+  end
 end
