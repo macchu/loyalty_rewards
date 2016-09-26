6e4c47603c1752d6dee5c49fac74d5b72e5aa919
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-09-26 12:21:23 -0500

    Trying to retain the verification code within the enroll_patron flow.

--- app/services/pre_check_in.rb
+++ app/services/pre_check_in.rb
@@ -4,13 +4,17 @@ module PreCheckIn
   class SMSCheckIn
     def initialize(check_in_message, email_of_store)
       Rails.logger.info " #{self.class.to_s}##{__method__.to_s}: email_of_store -> #{email_of_store}"
-      message = ParseEmailMessage.new(check_in_message)
+      
       store = Store.find_by_email_for_check_ins(email_of_store)
+      
+      message = ParseEmailMessage.new(check_in_message)
       patron = Patron.find_by_digit_only_phone_number(message.sender_local_part)
       case 
       when patron.nil?
         Rails.logger.info " #{self.class.to_s}##{__method__.to_s}: enroll_patron #{message.sender} "
-        enroller = EnrollPatron.start( phone_number: message.sender_local_part )
+        
+        EnrollPatron.start( phone_number: message.sender_local_part, patronage_proof:  )
+        
         PatronEnrollmentMailer.provide_name(message.sender).deliver_now
       
       when patron.pending
@@ -20,5 +24,11 @@ module PreCheckIn
       end
 
     end
+
+    def enroll_patron_params(message)
+      params = { phone_number: message.sender_local_part }
+      params << { patronage_proofs: { code: message.body_text_part} } if message.body_text_part
+    end
+
   end
 end
