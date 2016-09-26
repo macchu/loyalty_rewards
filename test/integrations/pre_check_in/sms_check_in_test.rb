8deeff9d24d728bd64dfe49bf281711c54fb81a0
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-09-26 12:02:08 -0500

    Tested email generation and Patron.pending, but failing to store patronage_proof.

--- test/integrations/pre_check_in/sms_check_in_test.rb
+++ test/integrations/pre_check_in/sms_check_in_test.rb
@@ -3,13 +3,13 @@ require 'test_helper'
 class PreCheckInTest
   class SMSCheckInTest < ActionDispatch::IntegrationTest
     def setup
+      @actionmailer_size_start = ActionMailer::Base.deliveries.size
       @new_patron_msg = Mail.new(fixture('unknown_sender_with_code'))
+      PreCheckIn::SMSCheckIn.new(@new_patron_msg,'france_44@stampstamp.com')
     end
 
     test 'A check in from a new patron generates an enrollment message.' do
-      assert_difference 'ActionMailer::Base.deliveries.size', +1 do
-        @controller = PreCheckIn::SMSCheckIn.new(@new_patron_msg,'france_44@stampstamp.com')
-      end
+      assert 1, ActionMailer::Base.deliveries.size - @actionmailer_size_start 
       invite_email = ActionMailer::Base.deliveries.last
    
       assert_equal '6124567890@vzwpix.com', invite_email.to[0]
@@ -18,11 +18,11 @@ class PreCheckInTest
     end
 
     test 'A check from a new patron creates a pending patron.' do
-
+      assert Patron.last.pending
     end
 
     test 'A pending patron retains the verification code from their purchase.' do
-
+      assert 'Z105', Patron.last.patronage_proof.last
     end
   end
 end
