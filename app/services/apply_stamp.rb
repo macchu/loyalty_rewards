45e0947cb73e402bbe1dfe4919fd3506464784da
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-10-02 16:21:59 -0500

    Don't send the card from ApplyStamp service, because single-responsibility.  Displaying the card means different things to a Web app and an SMS app.

--- app/services/apply_stamp.rb
+++ app/services/apply_stamp.rb
@@ -1,16 +1,14 @@
 class ApplyStamp
+  attr_reader :card
   def initialize(patron:, store:, check_in=nil)
     #Create a check in if necessary.
     check_in ||= CheckIn.create(patron: patron, store: store)
 
-
     #Get or create the LoyaltyCard.
-    loyalty_card = patron.find_or_create_loyalty_card(store)
-
+    @card = patron.find_or_create_loyalty_card(store)
 
     #Stamp the card.
-    loyalty_card.apply_stamp
+    @card.apply_stamp
 
-    #Send the loyalty card. 
   end
 end
