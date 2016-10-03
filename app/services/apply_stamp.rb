e69eca6bb29af747becc588d6b009125edb39340
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-10-03 10:42:44 -0500

    No need to default a named parameter to nil.  Just pass it nil.

--- app/services/apply_stamp.rb
+++ app/services/apply_stamp.rb
@@ -1,6 +1,6 @@
 class ApplyStamp
   attr_reader :card
-  def initialize(patron:, store:, check_in=nil)
+  def initialize(patron:, store:, check_in:)
     #Create a check in if necessary.
     check_in ||= CheckIn.create(patron: patron, store: store)
 
--- test/services/apply_stamp/apply_stamp_test.rb
+++ test/services/apply_stamp/apply_stamp_test.rb
@@ -2,14 +2,23 @@ require 'test_helper'
 
 class ApplyStampTest < ActiveSupport::TestCase
   def setup
-    @full_card = loyalty_cards(:full_card)
+    #Fill the card with stamps.
+    @loyalty_card_for_bill = loyalty_cards(:card_for_bill)
+    @loyalty_card_for_bill.stamp_count = @loyalty_card_for_bill.stamps_required
+    @loyalty_card_for_bill.save
+
+    @bill = @loyalty_card_for_bill.patron
+    @store = @loyalty_card_for_bill.store
+
   end
 
   test 'Verify setup' do
-    assert_equal 10, @full_card.stamp_count
+    assert_equal 10, @loyalty_card_for_bill.stamp_count
   end
 
   test 'A new loyalty card is created and stamped if the current card is full.' do
-    
+    assert_difference("LoyaltyCard.count", +1) do
+      ApplyStamp.new(patron: @bill, store: @store, check_in: nil)
+    end
   end
 end
