2adf1b7f12444d45ac775c20905db7a5ac6ba938
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-10-13 11:27:43 -0500

    Successfully tested GenerateRewardsCode.rb

--- app/services/generate_rewards_code.rb
+++ app/services/generate_rewards_code.rb
@@ -2,9 +2,7 @@ class GenerateRewardsCode
   attr_reader :code
   
   def initialize(loyalty_card)
-    store = loyalty_card.store
-    
-    case loyalty_card.redemption_type
+    case loyalty_card.redemption_code_type
     when :custom_formula
 
     when :qcode
@@ -12,13 +10,13 @@ class GenerateRewardsCode
     when :barcode
 
     when :alpha_numeric
-
+      @code = random_alpha_numeric_string_generator
     else
-      @code = random_alpha_numeric
+      @code = random_alpha_numeric_string_generator
     end
   end
 
-  def random_alpha_numericnumber_generator
+  def random_alpha_numeric_string_generator
     [*('a'..'z'),*('0'..'9')].shuffle[0,8].join
   end
 end
