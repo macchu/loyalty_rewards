c1066551bbf29d3346c67445d45b3871a530d687
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-10-27 11:52:46 -0500

    Successfully tested the RedemptionMailer.

--- app/mailers/redemption_mailer.rb
+++ app/mailers/redemption_mailer.rb
@@ -1,2 +1,9 @@
 class RedemptionMailer < ApplicationMailer
+  default from: "desking.board@gmail.com"
+
+  def send_link(recipient, redemption_url)
+    @redemption_url = redemption_url
+    mail(to: recipient)
+
+  end
 end
--- test/mailers/redemption_mailer_test.rb
+++ test/mailers/redemption_mailer_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class RedemptionMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Email gets sent' do
+     # Create the email and store it for further assertions
+    email = RedemptionMailer.send_link('me@example.com',
+                                           'http://www.example.com/redemptions/redeem/1234')
+ 
+    # Send the email, then test that it got queued
+    assert_emails 1 do
+      email.deliver_now
+    end
+ 
+    # Test the body of the sent email contains what we expect it to
+    #assert_equal '', email.subject
+    assert_includes 'Click this link when you are ready to redeem your reward: http://www.example.com/redemptions/redeem/1234.', email.body.to_s.strip
+    
+  end
+
 end
