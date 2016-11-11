750cc581d0e85ad7cdde7d08139c0cb02ed8de0b
Matt Rasmusson <matt.rasmusson@gmail.com>
2016-11-11 10:06:47 -0600

    Removed qrcode and barcode generation files.

--- app/services/generate_rewards_code.rb
+++ app/services/generate_rewards_code.rb
@@ -1,17 +1,9 @@
 class GenerateRewardsCode
   attr_reader :code
-  attr_reader :qrcode_image
   
   def initialize(loyalty_card)
     case loyalty_card.redemption_code_type
     when :custom_formula
-
-    when :qcode
-      @code = random_alpha_numeric_string_generator
-      qrcode = RQRCode::QRCode.new(@code)
-      @qrcode_image = qrcode.as_png
-
-    when :barcode
 
     when :random_alpha_numeric
       @code = random_alpha_numeric_string_generator
