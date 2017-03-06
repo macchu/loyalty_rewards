34af3452d20a35d6fc6f142d028893bd96d464eb
Matt Rasmusson <matt.rasmusson@gmail.com>
2017-03-06 13:18:24 -0600

    Removed debug statements.

--- app/services/stamped_card_interceptor.rb
+++ app/services/stamped_card_interceptor.rb
@@ -2,21 +2,9 @@ class StampedCardInterceptor
   class << self
  
     def delivering_email(mail)
-      ap "mail methods:"
-      ap mail.methods
-
-      ap mail.subject
-      # mail.parts.each_with_index do |part, i|
-      #   ap "part[#{i}]: #{part}"
-      # end
-      ap "content_id: #{mail.parts.last.content_id}"
+      #TODO: 1) Can content-id be set without an interceptor?  Possibly when the message is first mailed?
+      #      2) At least dynamically access the "part" containing the attachment.
       mail.parts.last.content_id = nil
-      ap "new content_id: #{mail.parts.last.content_id}"
-      ap "content_id: #{mail.content_id}"
-      ap "content_description: #{mail.content_description}"
-      ap "file_name: #{mail.filename}"
-
-      mail.content_id = nil
     end
  
   end
