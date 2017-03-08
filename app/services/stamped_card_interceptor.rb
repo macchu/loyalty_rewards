7e25362355a673a0f465805dfa2dbcbb0a35b7b8
Matt Rasmusson <matt.rasmusson@gmail.com>
2017-03-08 13:09:27 -0600

    Interceptor sets an email's content-id to nil in order to display attachments on iMessage.

--- app/services/stamped_card_interceptor.rb
+++ app/services/stamped_card_interceptor.rb
@@ -4,8 +4,12 @@ class StampedCardInterceptor
     def delivering_email(mail)
       #TODO: 1) Can content-id be set without an interceptor?  Possibly when the message is first mailed?
       #      2) At least dynamically access the "part" containing the attachment.
-      mail.parts.last.content_id = nil
+      
+      #The presence of content-id in the attachment part of the email, causes iMessage to choke and
+      #   not display the attached loyalty card.  Setting it to nil avoids this problem.
+      mail.parts.each do |part|
+        part.content_id = nil unless part.nil?
+      end
     end
- 
   end
 end
