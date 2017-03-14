12696d0fe14fd3730ad86e0de55cc33bd9473cbf
Matt Rasmusson <matt.rasmusson@gmail.com>
2017-03-14 12:18:15 -0500

    Made the content-id stripping logic more explicit.

--- app/services/stamped_card_interceptor.rb
+++ app/services/stamped_card_interceptor.rb
@@ -8,7 +8,13 @@ class StampedCardInterceptor
       #The presence of content-id in the attachment part of the email, causes iMessage to choke and
       #   not display the attached loyalty card.  Setting it to nil avoids this problem.
       mail.parts.each do |part|
-        part.content_id = nil unless part.nil?
+        if part.content_id.nil?
+          #Do nothing.
+          Rails.logger.info " #{self.class.to_s}##{__method__.to_s}: Content-ID is nil."
+        else
+          Rails.logger.info " #{self.class.to_s}##{__method__.to_s}: Content-ID: #{part.content_id}"
+          part.content_id = nil
+        end
       end
     end
   end
