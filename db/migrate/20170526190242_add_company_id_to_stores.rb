273feaf5375e1deffef466a9afc794c946ed1a03
Matt Rasmusson <matt.rasmusson@gmail.com>
2017-05-26 14:05:36 -0500

    Each Store may belong to a Company.

---
+++ db/migrate/20170526190242_add_company_id_to_stores.rb
@@ -0,0 +1,5 @@
+class AddCompanyIdToStores < ActiveRecord::Migration[5.0]
+  def change
+    add_column :stores, :company_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170525161544) do
+ActiveRecord::Schema.define(version: 20170526190242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -138,6 +138,7 @@ ActiveRecord::Schema.define(version: 20170525161544) do
     t.datetime "updated_at",              null: false
     t.string   "email_for_check_ins"
     t.integer  "redemption_code_type_id"
+    t.integer  "company_id"
     t.index ["email_for_check_ins"], name: "index_stores_on_email_for_check_ins", using: :btree
   end
 
