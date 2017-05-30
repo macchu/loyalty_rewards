feca8ea1781454a0195dda39ea65e95156d744d9
Matt Rasmusson <matt.rasmusson@gmail.com>
2017-05-30 13:31:59 -0500

    Removed Company ID from stores.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530182822) do
+ActiveRecord::Schema.define(version: 20170530183106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -134,7 +134,6 @@ ActiveRecord::Schema.define(version: 20170530182822) do
     t.datetime "updated_at",              null: false
     t.string   "email_for_check_ins"
     t.integer  "redemption_code_type_id"
-    t.integer  "company_id"
     t.index ["email_for_check_ins"], name: "index_stores_on_email_for_check_ins", using: :btree
   end
 
