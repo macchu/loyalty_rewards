e6f1c7a1b048c3446b1eb7854d6157a47a0c210f
Matt Rasmusson <matt.rasmusson@gmail.com>
2017-05-30 13:27:45 -0500

    Decided to remove Company.  It is an abstration that I am not sure that I need right now.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170526202553) do
+ActiveRecord::Schema.define(version: 20170530182407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,12 +39,6 @@ ActiveRecord::Schema.define(version: 20170526202553) do
     t.integer  "check_in_strategy_id"
     t.integer  "resulting_stamp_count"
     t.integer  "company_id"
-  end
-
-  create_table "companies", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "loyalty_card_terms", force: :cascade do |t|
