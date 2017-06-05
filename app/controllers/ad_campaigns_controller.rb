e28bda8f6b63af8bd637666f9bf747bd25a1cbf3
Matt Rasmusson <matt.rasmusson@gmail.com>
2017-06-05 16:39:24 -0500

    Added flash error messages and parameter whitelisting for AdCampaign.

--- app/controllers/ad_campaigns_controller.rb
+++ app/controllers/ad_campaigns_controller.rb
@@ -4,25 +4,30 @@ class AdCampaignsController < ApplicationController
   def new
     @store = Store.find(params[:store_id])
     @store.define_ad_campaign_targets
-    @campaign = @store.ad_campaigns.build( { ad_campaign_targets_attributes: [ { description: "NA", patron_id: @store.patrons.first.id },
-                                                                                { description: "NA2", patron_id: @store.patrons.last.id } ] })
+    @campaign = @store.ad_campaigns.build( { ad_campaign_targets_attributes: [ { patron_id: @store.patrons.first.id },
+                                                                                {  patron_id: @store.patrons.last.id } ] })
     #@campaign.ad_campaign_targets = @store.potential_ad_campaign_targets
   end
 
   def create
-
-    @campaign.ad_campaign_audience = @store.pending_ad_campaign_audience
+    #byebug
+    @campaign = AdCampaign.new(ad_campaign_params)
+    @store = @campaign.store
+    
     if @campaign.save
-      flash[:notice] = "Campaign updated."
-      redirect_to [@store, @campaign]
+      flash[:success] = "Campaign updated."
+      render :edit
     else
       flash[:error] = "Failed to create campaign."
+      render :new
     end
   end
 
   def edit
     @store = Store.find(params[:store_id])
     @campaign = AdCampaign.find(:campaign_id)
+
+
 
   end
 
@@ -31,5 +36,11 @@ class AdCampaignsController < ApplicationController
   end
 
   private
+   def ad_campaign_params
+      # It's mandatory to specify the nested attributes that should be whitelisted.
+      # If you use `permit` with just the key that points to the nested attributes hash,
+      # it will return an empty hash.
+      params.require(:ad_campaign).permit(:store_id, :platform_id, :description, ad_campaign_targets_attributes: [ :patron_id ])
+    end
 
 end
