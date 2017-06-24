44355d2dedacddab34a30a5d21312da5cd12bb61
Matt Rasmusson <matt.rasmusson@gmail.com>
2017-06-24 07:52:53 -0500

    Randomizing the social media checks.

--- app/helpers/hero_page_helpers.rb
+++ app/helpers/hero_page_helpers.rb
@@ -1,5 +1,10 @@
 module HeroPagesHelpers
   def generate_image_tag(image_name)
-
+    zero_or_one = rand(2)
+    if zero_or_one == 0
+      return true
+    else
+      return false
+    end
   end
 end
